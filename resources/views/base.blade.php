4fb77836df24590fb52a938dcd9ceca76c6ff62b
arieansyah <arieansyahp.bun@gmail.com>
2018-09-04 22:12:35 +0700

    add admin lte

---
+++ resources/views/base.blade.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>TravelNeo</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <link rel="icon" href="{{url('img/logo.png')}}" type="image/x-icon">
+
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="{{ asset('dist/css/skins/_all-skins.min.css') }}">
+
+  <link rel="stylesheet" href="{{ asset('plugins/iCheck/all.css') }}">
+
+  {{-- dataTables css --}}
+  <link rel="stylesheet" type="text/css" href="{{ asset('plugins/datatables/dataTables.bootstrap.css') }}">
+
+  {{-- datepicker --}}
+  <link rel="stylesheet" type="text/css" href="{{ asset('plugins/datepicker/datepicker3.css') }}">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition skin-blue-light sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="#" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><img src="{{asset('img/logo50.png')}}"></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+
+          <li><a href="{{url('logout')}}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+          <!-- Control Sidebar Toggle Button -->
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <!-- =============================================== -->
+
+  <!-- Left side column. contains the sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="treeview">
+          <a href="{{url('user')}}">
+            <i class="fa fa-home"></i> <span>Home</span>
+          </a>
+        </li>
+          <li class="treeview">
+            <a href="{{url('user')}}">
+              <i class="fa fa-users"></i> <span>User</span>
+            </a>
+          </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- =============================================== -->
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        @yield('title')
+      </h1>
+      <ol class="breadcrumb">
+        @section('breadcrumb')
+        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        @show
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      @yield('content')
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      TravelNeo
+    </div>
+    <strong> Created by: TravelNeo</strong>
+  </footer>
+
+  <!-- jQuery 2.2.3 -->
+  <script src="{{ asset('plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+  <!-- Bootstrap 3.3.6 -->
+  <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+  <!-- SlimScroll -->
+  <script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+  <!-- FastClick -->
+  <script src="{{ asset('plugins/fastclick/fastclick.js') }}"></script>
+  <!-- AdminLTE App -->
+  <script src="{{ asset('dist/js/app.min.js') }}"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="{{ asset('dist/js/demo.js') }}"></script>
+  {{-- datatables --}}
+  <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables/dataTables.bootstrap.min.js') }}"></script>
+  {{-- datepicker --}}
+  <script src="{{ asset('plugins/datepicker/bootstrap-datepicker.js') }}"></script>
+  {{-- validator.js --}}
+  <script src="{{ asset('js/validator.js') }}"></script>
+
+@yield('script')
+</body>
+</html>
